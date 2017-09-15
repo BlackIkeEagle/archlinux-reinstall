b57ad7c1b9e1ecffa9fb843073423cadcf918853
BlackEagle <ike.devolder@gmail.com>
2017-09-15 17:16:26 +0200

    allow to create a package list with base package excluded
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- create-package-list.sh
+++ create-package-list.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
 cd "$(dirname "$(readlink -f "$0")")"
-pacman -Qeq | grep -v -f base-packages.txt
+grepextra=""
+if [[ ! -z $1 ]]; then
+    for file in "$@"; do
+        grepextra="$grepextra -f $file"
+    done
+fi
+pacman -Qeq | grep -v -f base-packages.txt$grepextra
