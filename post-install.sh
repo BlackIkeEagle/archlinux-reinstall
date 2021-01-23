548cf440659f279551b3a3f63f7c88d9243c8b07
BlackEagle <ike.devolder@gmail.com>
2021-01-23 09:37:51 +0100

    stop using tlp on desktop by default
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- post-install.sh
+++ post-install.sh
@@ -85,12 +85,6 @@ fi
 if which smartd > /dev/null 2>&1; then
     systemctl enable smartd.service
 fi
-if which tlp > /dev/null 2>&1; then
-    systemctl mask systemd-rfkill.service
-    systemctl mask systemd-rfkill.socket
-    systemctl enable tlp.service
-    systemctl enable tlp-sleep.service
-fi
 if which sddm > /dev/null 2>&1; then
     systemctl enable sddm.service
 fi
