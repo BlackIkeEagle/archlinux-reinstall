74958d0c817d0c3272f419ebb75c7faa39aa5784
BlackEagle <ike.devolder@gmail.com>
2024-03-11 11:34:47 +0100

    update plasma-desktop for plasma6
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

---
+++ install-desktop-plasma.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -e
+
+./install-desktop.sh desktop.packages plasma-desktop.packages "$@"
--- install-desktop-plasma5.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-./install-desktop.sh desktop.packages plasma5-desktop.packages "$@"
