c4623ccbf0ac8c70a60b98fe28258e711929d0ce
BlackEagle <ike.devolder@gmail.com>
2024-03-11 11:04:32 +0100

    remove kdepim install/remove helper scripts
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- kdepim-remove.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo pacman -Rcs akonadiconsole akonadi-calendar-tools akonadi-import-wizard grantlee-editor itinerary kaddressbook kalarm kdepim-addons kdepim-runtime kleopatra kmail kmail-account-wizard kontact korganizer mbox-importer pim-data-exporter pim-sieve-editor zanshin postgresql postgresql-old-upgrade
--- kdepim.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo pacman -S akonadiconsole akonadi-calendar-tools akonadi-import-wizard grantlee-editor itinerary kaddressbook kalarm kdepim-addons kdepim-runtime kleopatra kmail kmail-account-wizard kontact korganizer mbox-importer pim-data-exporter pim-sieve-editor zanshin postgresql postgresql-old-upgrade --assume-installed mariadb
-
-mkdir -p ~/.config/akonadi
-cat <<EOL > ~/.config/akonadi/akonadiserverrc
-[%General]
-Driver=QPSQL
-EOL
